3eda1edd495efd19e37e445ac526fdd283f78b38
John Glass <jcglass2001@gmail.com>
2022-06-20 06:06:33 -0400

    Added changes to HelloDolly

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -1,5 +1,9 @@
 package ver1;
 
 public class HelloDolly {
-
+	public static void main(String[]args) {
+		System.out.println("hello dolly");
+		System.out.println("Hello Dollly from Eclipse Again");
+	}
+	
 }
